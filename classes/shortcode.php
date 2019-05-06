2305bb61bca6c10bb84374c294784d89e8687e90
sebastienserre <sebastien@thivinfo.com>
2019-05-06 15:40:40 +0200

    rewrite shortcode

--- classes/shortcode.php
+++ classes/shortcode.php
@@ -1,6 +1,8 @@
 <?php
 
 
+use MESSESINFO\API\Messesinfo;
+
 class thfo_messeinfo_shortcode {
 	function __construct() {
 		add_shortcode( 'messesinfo', array( $this, 'messesinfo_shortcode' ) );
@@ -9,93 +11,78 @@ class thfo_messeinfo_shortcode {
 
 	public function messesinfo_shortcode( $atts ) {
 
-		$a = shortcode_atts( array( 'id' => 'plaisir 78', 'result' => '5' ), $atts );
+		shortcode_atts(
+			array(
+				'search',
+				'localityId' => '78/plaisir/saint-pierre',
+				'result'     => '5',
+			),
+			$atts
+		);
 
-		$id = esc_html( $atts['id'] );
-
-	//	$id             = explode( '/', $atts['id'] );
+		$localityId     = esc_html( $atts['localityid'] );
 		$atts['result'] = intval( $atts['result'] );
 
-		/**
-		 * Transient deletion if shortcode params changed
-		 */
+		$mass = Messesinfo::get_mass_data( $localityId );
 
-		$search_old = get_option('messeinfo_search_shortcode' );
+		$i = 1;
 
-		if ( ! $search_old || $search_old =! $atts['id'] ){
-			update_option('messeinfo_search_shortcode', $atts['id'] );
-		}
-
-		if ($search_old != $atts['id']) { delete_transient('messesinfo_data_sc'); }
-
-		/**
-		 * Transient creation to store data 24h
-		 */
-
-		$url = "http://www.messes.info/api/v2$id?userkey=messesinfo&format=json";
-		$mass = get_transient( 'messesinfo_data_sc' );
-		if ( ! $mass ) {
-			//$url  = file_get_contents( "http://www.messes.info/api/v2/horaires/" . $id[0] . "%20" . $id[1] . "?userkey=messesinfo&format=json" );
-			$url  = file_get_contents( $url );
-			$mass = json_decode( $url, true );
-			set_transient( 'messesinfo_data_sc', $mass, 86400 );
-		}
-
-		$shortcode = '';
-		if ( isset( $mass['errorMessage'] ) ) {
-			echo '<p>' . $mass['errorMessage'] . '</p>';
-		} else {
-			foreach ( $mass as $m ) {
-				if ( is_array( $m ) ) {
-					$i = 1;
-					foreach ( $m as $mess ) {
-						if ( $i <= $atts['result'] ){
-				//		for ( $i = 0; $i <= $atts['result']; $i++ ) {
-							$newdate   = date_timestamp_get( date_create( $mess['date'] ) );
-							$shortcode .= '<div class="messeinfo messeinfo-' . $i . '">
-                                <div class="mass-infos mass-infos-' . $i . '">
-                                    <p class="mass-date mass-date-' . $i . '">
-                                        <strong>' . date_i18n( 'l d F Y', $newdate ) . ' - ' . $mess['time'] . '</strong>
-                                    </p>
-
-								    ';
-							if ( $mess['timeType'] === 'SUNDAYMASS' ) {
-								$type      = __( 'Weekly Mass', 'messesinfo' );
-								$shortcode .= '<p>' . $type . '</p>';
-							};
-
-							$shortcode .= '<a href="http://egliseinfo.catholique.fr/lieu/' . $mess['locality']['id'] . '"
-                                       target="_blank">
-                                        <p>' . $mess['locality']['type'] . ' ' . $mess['locality']['name'] . '</p>
-                                    </a>
-
-                                    <p>' . $mess['locality']['address'] . $mess['locality']['zipcode'] . ' ' . $mess['locality']['city'] . '</p>
-
-                                    <a href="http://egliseinfo.catholique.fr/communaute/' . $mess['communityId'] . '"
-                                       target="_blank">
-                                        <p>' . $mess['locality']['sectorType'] . ' de ' . $mess['locality']['sector'] . '</p>
-                                    </a>
-
-                                </div>
-
-                            </div>
-                            <div class="clear"></div>';
-						}
-
-						$i++;
-					}
-				}
+		ob_start();
+		foreach ( $mass->celebrationsTime as $mess ) {
+			$newdate = date_timestamp_get( date_create( $mess->date ) );
+			if ( $mess->timeType === 'SUNDAYMASS' ) {
+				$type = __( 'Weekly Mass', 'messesinfo' );
 			}
+			$locality = Messesinfo::get_locality_info( $localityId );
+			?>
+            <div class="messeinfo messeinfo-<?php echo $i ?>">
+                <div class="messesinfo-left">
+                    <div class="mass-infos mass-infos-<?php echo $i ?>">
+                        <p class="mass-date mass-date-<?php echo $i ?>">
+                            <strong><?php echo date_i18n( 'l d F Y', $newdate ) . ' - ' . $mess->time ?></strong>
+							<?php echo $type ?>
+                        </p>
+                        <div class="messesinfo-adress">
+                            <p><?php echo $locality->type . ' ' . $locality->commonName; ?> </p>
+                            <p><?php echo $locality->address->street; ?> </p>
+                            <p><?php echo $locality->address->zipCode . ' ' . $locality->address->city; ?></p>
+                        </div>
 
+                        <p><a href="http://egliseinfo.catholique.fr/lieu/<?php echo $mess->localityId ?>"
+                              target="_blank">
+								<?php _e( 'Link to the church', 'messesinfo' ); ?>
+                            </a> -
+                            <a href="http://egliseinfo.catholique.fr/communaute/<?php echo $mess->communityId ?>"
+                               target="_blank">
+								<?php _e( 'Link to the community', 'messesinfo' ); ?>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+	            <?php
+	            if ( ! empty( $locality->picture ) ) { ?>
+                    <div class="messesinfo-right">
+                        <img src="<?php echo $locality->picture ?>">
+                    </div>
+		            <?php
+	            }
+                    ?>
+            </div>
+            <div class="clear"></div>
+			<?php
+			$i ++;
 		}
 
+
+		$shortcode = ob_get_clean();
 		$shortcode .= messesinfos_widget::messesinfo_promote();
+
 		return $shortcode;
 	}
 
-	public function messesinfo_shortcode_search($atts){
+	public function messesinfo_shortcode_search( $atts ) {
 
-		$a = shortcode_atts( array( 'result' => '5' ), $atts );
+		$a              = shortcode_atts( array( 'result' => '5' ), $atts );
 		$atts['result'] = intval( $atts['result'] );
 
 		/**
@@ -106,8 +93,8 @@ class thfo_messeinfo_shortcode {
 		$shortcode .= '<input type="submit" name="messesinfo_search_submit" >';
 		$shortcode .= '</form>';
 
-		$search_field = sanitize_text_field($_POST['messesinfo_search_field']);
-		$url =file_get_contents( 'http://www.messes.info/api/v2/horaires/'. $search_field .'?userkey=messesinfos&format=json');
+		$search_field = sanitize_text_field( $_POST['messesinfo_search_field'] );
+		$url          = file_get_contents( 'http://www.messes.info/api/v2/horaires/' . $search_field . '?userkey=messesinfos&format=json' );
 
 		$mass = json_decode( $url, true );
 
@@ -119,7 +106,7 @@ class thfo_messeinfo_shortcode {
 					$i = 1;
 					foreach ( $m as $mess ) {
 						//var_dump($mess);
-						if ( $i <= $atts['result'] ){
+						if ( $i <= $atts['result'] ) {
 							//		for ( $i = 0; $i <= $atts['result']; $i++ ) {
 							$newdate   = date_timestamp_get( date_create( $mess['date'] ) );
 							$shortcode .= '<div class="messeinfo messeinfo-' . $i . '">
@@ -146,7 +133,7 @@ class thfo_messeinfo_shortcode {
                                         <p>' . $mess['locality']['sectorType'] . ' de ' . $mess['locality']['sector'] . '</p>
                                     </a>
                                     <div class="acf-map">
-	<div class="marker" data-lat="'. $mess['locality']['latitude'].'" data-lng="'. $mess['locality']['longitude'].'"></div>
+	<div class="marker" data-lat="' . $mess['locality']['latitude'] . '" data-lng="' . $mess['locality']['longitude'] . '"></div>
 </div>
 
                                 </div>
@@ -155,7 +142,7 @@ class thfo_messeinfo_shortcode {
                             <div class="clear"></div>';
 						}
 
-						$i++;
+						$i ++;
 					}
 				}
 			}
@@ -165,7 +152,7 @@ class thfo_messeinfo_shortcode {
 		return $shortcode;
 	}
 
-	public function messesinfos_display_results($mass){
+	public function messesinfos_display_results( $mass ) {
 		if ( isset( $mass['errorMessage'] ) ) {
 			echo '<p>' . $mass['errorMessage'] . '</p>';
 		} else {
@@ -173,8 +160,8 @@ class thfo_messeinfo_shortcode {
 				if ( is_array( $m ) ) {
 					$i = 1;
 					foreach ( $m as $mess ) {
-						var_dump($mess);
-						if ( $i <= $atts['result'] ){
+						var_dump( $mess );
+						if ( $i <= $atts['result'] ) {
 							//		for ( $i = 0; $i <= $atts['result']; $i++ ) {
 							$newdate   = date_timestamp_get( date_create( $mess['date'] ) );
 							$shortcode .= '<div class="messeinfo messeinfo-' . $i . '">
@@ -207,7 +194,7 @@ class thfo_messeinfo_shortcode {
                             <div class="clear"></div>';
 						}
 
-						$i++;
+						$i ++;
 					}
 				}
 			}
