9cd3457de13e5529cf906382fdd8ccbd1c65370e
sebastienserre <sebastien@thivinfo.com>
2019-05-06 19:40:57 +0200

    add error msg if no mass

--- classes/shortcode.php
+++ classes/shortcode.php
@@ -28,61 +28,69 @@ class thfo_messeinfo_shortcode {
 
 		$mass = Messesinfo::get_mass_data( $localityId );
 
+
 		$i = 1;
 		$b = 1;
 
 		ob_start();
-		foreach ( $mass->celebrationsTime as $mess ) {
-			if ( $i <= $atts['result'] ) {
-				$newdate = date_timestamp_get( date_create( $mess->date ) );
-				switch ( $mess->timeType ){
-                    case 'SUNDAYMASS':
-	                    $type = __( 'Weekly Mass', 'messesinfo' );
-	                    break;
-                    case 'WEEKMASS':
-	                    $type = __( 'Daily Mass', 'messesinfo' );
-	                    break;
 
-                }
+		if ( empty( $mass->celebrationsTime ) ){
+			?>
+            <p> <?php _e('No mass scheduled', 'messesinfo' ); ?> </p>
+            <?php
+		} else {
+			foreach ( $mass->celebrationsTime as $mess ) {
+				if ( $i <= $atts['result'] ) {
+					$newdate = date_timestamp_get( date_create( $mess->date ) );
+					switch ( $mess->timeType ) {
+						case 'SUNDAYMASS':
+							$type = __( 'Weekly Mass', 'messesinfo' );
+							break;
+						case 'WEEKMASS':
+							$type = __( 'Daily Mass', 'messesinfo' );
+							break;
 
-				$locality = Messesinfo::get_locality_info( $localityId );
-				?>
-                <div class="messeinfo messeinfo-<?php echo $i ?>">
-                    <div class="messesinfo-left">
-                        <div class="mass-infos mass-infos-<?php echo $i ?>">
-                            <p class="mass-date mass-date-<?php echo $i ?>">
-                                <strong><?php echo date_i18n( 'l d F Y', $newdate ) . ' - ' . $mess->time ?></strong>
-								<?php echo $type ?>
-                            </p>
-                            <div class="messesinfo-adress">
-                                <p><?php echo $locality->type . ' ' . $locality->commonName; ?> </p>
-                                <p><?php echo $locality->address->street; ?> </p>
-                                <p><?php echo $locality->address->zipCode . ' ' . $locality->address->city; ?></p>
+					}
+
+					$locality = Messesinfo::get_locality_info( $localityId );
+					?>
+                    <div class="messeinfo messeinfo-<?php echo $i ?>">
+                        <div class="messesinfo-left">
+                            <div class="mass-infos mass-infos-<?php echo $i ?>">
+                                <p class="mass-date mass-date-<?php echo $i ?>">
+                                    <strong><?php echo date_i18n( 'l d F Y', $newdate ) . ' - ' . $mess->time ?></strong>
+									<?php echo $type ?>
+                                </p>
+                                <div class="messesinfo-adress">
+                                    <p><?php echo $locality->type . ' ' . $locality->commonName; ?> </p>
+                                    <p><?php echo $locality->address->street; ?> </p>
+                                    <p><?php echo $locality->address->zipCode . ' ' . $locality->address->city; ?></p>
+                                </div>
+
+                                <p><a href="http://egliseinfo.catholique.fr/lieu/<?php echo $mess->localityId ?>"
+                                      target="_blank">
+										<?php _e( 'Link to the church', 'messesinfo' ); ?>
+                                    </a> -
+                                    <a href="http://egliseinfo.catholique.fr/communaute/<?php echo $mess->communityId ?>"
+                                       target="_blank">
+										<?php _e( 'Link to the community', 'messesinfo' ); ?>
+                                    </a>
+                                </p>
                             </div>
-
-                            <p><a href="http://egliseinfo.catholique.fr/lieu/<?php echo $mess->localityId ?>"
-                                  target="_blank">
-									<?php _e( 'Link to the church', 'messesinfo' ); ?>
-                                </a> -
-                                <a href="http://egliseinfo.catholique.fr/communaute/<?php echo $mess->communityId ?>"
-                                   target="_blank">
-									<?php _e( 'Link to the community', 'messesinfo' ); ?>
-                                </a>
-                            </p>
-                        </div>
-                    </div>
-					<?php
-					if ( ! empty( $locality->picture ) ) { ?>
-                        <div class="messesinfo-right">
-                            <img src="<?php echo $locality->picture ?>">
                         </div>
 						<?php
-					}
-					?>
-                </div>
-                <div class="clear"></div>
-				<?php
-				$i ++;
+						if ( ! empty( $locality->picture ) ) { ?>
+                            <div class="messesinfo-right">
+                                <img src="<?php echo $locality->picture ?>">
+                            </div>
+							<?php
+						}
+						?>
+                    </div>
+                    <div class="clear"></div>
+					<?php
+					$i ++;
+				}
 			}
 		}
 
